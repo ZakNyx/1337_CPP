83cba94cfab31c6ae67fa8777888bff1609623a7
Zakaria Ihirri <zihirri@e1r9p9.1337.ma>
2022-08-11 17:02:05 +0100

    =

--- cpp_06/ex01/Serialization.cpp
+++ cpp_06/ex01/Serialization.cpp
@@ -6,12 +6,16 @@
 /*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 21:23:39 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/11 16:52:31 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/08/11 17:01:59 by zihirri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serialization.hpp"
 
-uintptr_t serialize(Data* ptr){
-    return  (reinterpret_cast<std::uintptr_t>(ptr));
+uintptr_t   serialize(Data* ptr){
+	return (reinterpret_cast<std::uintptr_t>(ptr));
+}
+
+Data*		deserialize(uintptr_t raw){
+	return (reinterpret_cast<Data *>(raw));
 }
--- cpp_06/ex01/main.cpp
+++ cpp_06/ex01/main.cpp
@@ -6,17 +6,18 @@
 /*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 21:22:06 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/11 16:54:09 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/08/11 16:57:49 by zihirri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serialization.hpp"
 
-uintptr_t serialize(Data* ptr);
+uintptr_t   serialize(Data* ptr);
+Data*		deserialize(uintptr_t raw);
 
 int main( void )
 {
-    Data *ptr;
-    
-    std::cout << serialize(ptr) << std::endl;
+	Data *ptr = NULL;
+	
+	std::cout << serialize(ptr)          << std::endl;
 }
