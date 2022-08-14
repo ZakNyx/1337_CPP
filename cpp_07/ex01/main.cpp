cde67f7f4572ab02bd1d4012cfc3db76a9023290
ZakNyx <gobuwu@hotmail.com>
2022-08-14 18:38:41 +0100

    =

--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -3,16 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: zak <zak@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 15:18:57 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/13 17:10:15 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/08/14 17:05:57 by zak              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-
-
 
 void str_toupper(std::string str)
 {
