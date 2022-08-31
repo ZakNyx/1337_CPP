c46711ccf3865e28200a201d467541f407b31ea8
Zakaria Ihirri <zihirri@e1r10p8.1337.ma>
2022-08-31 11:40:30 +0100

    =

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -6,16 +6,18 @@
 /*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:27:34 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/30 21:54:32 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/08/31 11:39:23 by zihirri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EAZYFIND_HPP
 
-template < typename T >
-void    easyfind(T _haystack, int _needle){
-    
+#include <algorithm>
+template <typename T>
+void	easyfind(T _haystack, int _needle){
+	T<int>::iterator it = std::find_if(_haystack.begin(), _haystack.end(), _needle);
+	std::cout << *it << std::endl;
 };
 
 #endif
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:27:39 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/30 18:27:39 by zihirri          ###   ########.fr       */
+/*   Updated: 2022/08/31 11:40:27 by zihirri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include "easyfind.hpp"
+#include <vector>
+
+int main(void ){
+      std::vector<int> myvector;
+
+  myvector.push_back(10);
+  myvector.push_back(25);
+  myvector.push_back(40);
+  myvector.push_back(55);
+  easyfind(myvector, 40);
+}
