8f81b04c9804c3569384454639143f9695026cf2
ZakNyx <gobuwu@hotmail.com>
2022-09-02 23:41:55 +0100

    =

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -3,69 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zihirri <zihirri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: zak <zak@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/30 10:25:52 by zihirri           #+#    #+#             */
-/*   Updated: 2022/08/30 16:24:54 by zihirri          ###   ########.fr       */
+/*   Created: 2022/08/17 17:24:59 by zak               #+#    #+#             */
+/*   Updated: 2022/08/17 17:52:16 by zak              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARRAY_HPP
-# define ARRAY_HPP
+#include <iostream>
+#include <string>
 
-template <typename T>
+template <class T>
 class Array{
-	
 	private:
-		T				*_array;
-		unsigned int	_size;
+	
 	public:
-		Array( void ) : _array(new T[0]()), _size(0){};
-
-		Array(unsigned int n): _array(new T[n]()), _size(n){};
-
-		Array(Array const & _p1){
-			unsigned int _s = 0;
-			this->_array = new T[_p1._size];
-			this->_size = _p1._size;
-			while (_s < _p1._size){
-				this->_array[_s] = _p1._array[_s];
-				_s++;
-			}
-		};
-		
-		Array & operator = (Array const & _p1){
-			unsigned int _s = 0;
-			this->_array = new T[_p1._size];
-			this->_size = _p1._size;
-			while (_s < _p1._size){
-				this->_array[_s] = _p1._array[_s];
-				_s++;
-			}
-			return (this);
-		};
-		
-		class Objection : public std::exception{
-			public :
-				virtual const char * what() const throw(){
-					return ("The Index is out of bound !");
-				};
-		};
-		
-		T & operator [] ( unsigned int position){
-			if( this->_size <= position ){
-				throw Objection();
-			}
-			return (this->_array[position]);
-		}
-		
-		unsigned int	getSize( void ){
-			return (this->_size);
-		};
-		
-		~Array( void ){
-			delete this->_array;
-		};
+		Array( void );
+		~Array( void );
 };
-
-#endif
